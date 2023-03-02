28c2c54f71d97c728228a02c80a3f921513f0ba5
Gabriel Coronado <gcoronado5100@gmail.com>
2023-03-01 21:26:34 -0400

    lo que sea, usted me dijo

--- src/config.ts
+++ src/config.ts
@@ -5,13 +5,16 @@ import { fileURLToPath } from "url";
 const PORT = 3000;
 const app = express();
 
+
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("port", process.env.PORT || PORT);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.json());
+
 
 export default app;
--- src/controllers/subjectDetailController.ts
+++ src/controllers/subjectDetailController.ts
@@ -1,8 +1,8 @@
 import { Request, Response } from "express";
-import Subject from "../models/Subject.js";
+// import Subject from "../models/Subject.js";
 
 export const getSubjectDetail = async (_: Request, res: Response) => {
-  const subjects = await Subject.findAll();
+  // const subjects = await Subject.findAll();
   res.render("subjectDetail");
-  res.json(subjects);
+  // res.json(subjects);
 };
